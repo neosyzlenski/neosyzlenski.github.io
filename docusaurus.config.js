5f3c713e792ef614fce8dc1cc798f5a459c07464
Kapil Sharma <kapsharma@expediagrou.com>
2021-05-23 19:24:14 +0530

    Updating title and tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Kapil Sharma',
+  tagline: 'Tech Tinkerer',
   url: 'https://neosyzlenski.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -11,7 +11,7 @@ module.exports = {
   projectName: 'neosyzlenski.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'KS',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
